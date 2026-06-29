341ef7d39ece71a1f6416ae0be17b0751bdd2eda
Federico Becker <matiasbecker09@gmail.com>
2026-06-29 20:36:05 -0300

    subo home-page HTML+ CSS+ logica

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Auth, onAuthStateChanged } from '@angular/fire/auth';
+import { IonContent, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { LogoAppComponent } from 'src/app/components/logo-app/logo-app.component';
+import { AuthService } from 'src/app/services/auth.service';
+import { Usuario } from 'src/app/models';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [IonContent, IonButton, IonIcon, RouterLink, LogoAppComponent],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  private auth = inject(Auth);
+  private authService = inject(AuthService);
+
+  usuario: Usuario | null = null;
+  cargando = true;
+
+  ngOnInit() {
+    onAuthStateChanged(this.auth, async (user) => {
+      if (user) {
+        this.usuario = await this.authService.usuarioActual();
+      } else {
+        this.usuario = null;
+      }
+      this.cargando = false;
+    });
+  }
+
+  async cerrarSesion() {
+    await this.authService.logout();
+    this.usuario = null;
+  }
 }

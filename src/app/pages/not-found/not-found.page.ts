80b3989d3195bc38cde98c4b2f0f5e6278c2e826
Federico Becker <matiasbecker09@gmail.com>
2026-06-30 21:20:48 -0300

    subo HTML+CSS+logica

--- src/app/pages/not-found/not-found.page.ts
+++ src/app/pages/not-found/not-found.page.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonContent } from '@ionic/angular/standalone';
+import { LogoAppComponent } from 'src/app/components/logo-app/logo-app.component';
+import { BotonAccionComponent } from 'src/app/components/botones/boton-accion/boton-accion.component';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.page.html',
   styleUrls: ['./not-found.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, LogoAppComponent, BotonAccionComponent],
 })
-export class NotFoundPage implements OnInit {
+export class NotFoundPage {
+  private router = inject(Router);
 
-  constructor() { }
-
-  ngOnInit() {
+  irACatalogo() {
+    this.router.navigate(['/demo']);
   }
 
+  irAInicio() {
+    this.router.navigate(['/']);
+  }
 }

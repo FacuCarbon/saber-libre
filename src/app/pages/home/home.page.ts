14c0bad419b1e240687e0cc288548f86c487c767
Federico Becker <matiasbecker09@gmail.com>
2026-07-01 00:43:25 -0300

    fix de scroll horizontal

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,19 +1,15 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { Auth, onAuthStateChanged } from '@angular/fire/auth';
 import {
   IonContent,
   IonButton,
   IonIcon,
-  IonSpinner,
-  IonPopover,
-  IonList,
-  IonItem,
-  IonLabel,
 } from '@ionic/angular/standalone';
+import { HeaderComponent } from 'src/app/components/header/header.component';
 import { LogoAppComponent } from 'src/app/components/logo-app/logo-app.component';
 import { AuthService } from 'src/app/services/auth.service';
-import { RolUsuario, Usuario } from 'src/app/models';
+import { Usuario } from 'src/app/models';
 
 @Component({
   selector: 'app-home',
@@ -24,52 +20,24 @@ import { RolUsuario, Usuario } from 'src/app/models';
     IonContent,
     IonButton,
     IonIcon,
-    IonSpinner,
-    IonPopover,
-    IonList,
-    IonItem,
-    IonLabel,
     RouterLink,
+    HeaderComponent,
     LogoAppComponent,
   ],
 })
 export class HomePage implements OnInit {
   private auth = inject(Auth);
   private authService = inject(AuthService);
-  private router = inject(Router);
 
   usuario: Usuario | null = null;
-  rol: RolUsuario | null = null;
-  cargando = true;
 
   ngOnInit() {
     onAuthStateChanged(this.auth, async (user) => {
       if (user) {
         this.usuario = await this.authService.usuarioActual();
-        this.rol = await this.authService.rolUsuarioActual();
       } else {
         this.usuario = null;
-        this.rol = null;
       }
-      this.cargando = false;
     });
-  }
-
-  get puedeVerDashboard(): boolean {
-    return this.rol === 'administrador' || this.rol === 'bibliotecario';
-  }
-
-  async irADashboard() {
-    await this.router.navigate(['/dashboard']);
-  }
-
-  async irAPerfil() {
-    await this.router.navigate(['/dashboard/perfil']);
-  }
-
-  async cerrarSesion() {
-    await this.authService.logout();
-    this.usuario = null;
-    this.rol = null;
   }
 }
